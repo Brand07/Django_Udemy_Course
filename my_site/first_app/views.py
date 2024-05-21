00f37915d8c6d433692bae0c26908984f838e3a8
Brand07 <81128304+Brand07@users.noreply.github.com>
2024-05-20 22:56:32 -0500

    Try/Except for null pages

--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, HttpResponseNotFound
 
 
 # Create your views here
@@ -12,7 +12,12 @@ articles = {
 
 # Topic needs to be dynamically informed
 def news_view(request, topic):
-    return HttpResponse(articles[topic])
+    try:
+        result = articles[topic]
+        return HttpResponse(articles[topic])
+    except:
+        result = "No page for that topic"
+        return HttpResponseNotFound(result)
 
 def add_view(request, num1, num2):
     # domain.com/first_app/num1/num2 --> num1+num2
