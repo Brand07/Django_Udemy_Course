5184f46981ba28715263dab50b0ac300c7890b53
Brand07 <81128304+Brand07@users.noreply.github.com>
2024-05-22 22:54:00 -0500

    urls.py under main project

--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def example_view(request):
+    # path - my_app/templates/my_app/example.html
+    return render(request, 'my_app/example.html')
