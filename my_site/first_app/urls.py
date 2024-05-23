215b86469560b89b3ad302ea966a4d9cc4d4cbca
Brand07 <81128304+Brand07@users.noreply.github.com>
2024-05-22 22:16:15 -0500

    Templates
    Hard-coded views have been deleted. Starting on template .html files in place of.

--- my_site/first_app/urls.py
+++ my_site/first_app/urls.py
@@ -6,11 +6,6 @@ from . import views
 
 # Dynamic Routing
 urlpatterns = [
-    path('<int:num_page>', views.num_page_view),
-    # Path converter 'str' informs Django that
-    # The topic will always be a string
-    path('<str:topic>/', views.news_view, name='topic-page'),
-    # Likewise, the topic will be an int.
-    path('<int:num1>/<int:num2>', views.add_view),
+    path('', views.simple_view) # domain.com/first_app
 
 ]
--- my_site/first_app/views.py
+++ my_site/first_app/views.py
@@ -5,32 +5,7 @@ from django.urls import reverse
 
 # Create your views here
 
-articles = {
-    'sports' : 'Sports Page',
-    'finance' : 'Finance Page',
-    'politics' : 'Politics Page'
-}
-
-# Topic needs to be dynamically informed
-def news_view(request, topic):
-    try:
-        result = articles[topic]
-        return HttpResponse(articles[topic])
-    except:
-        result = "No page for that topic"
-        raise Http404("404 GENERIC ERROR!")
-
-def add_view(request, num1, num2):
-    # domain.com/first_app/num1/num2 --> num1+num2
-    result = num1 + num2
-    return HttpResponse(str(result))
-
-# domain.com/first_app/0 ---> domain.com/first_app/finance or sports, etc
-
-def num_page_view(request, num_page):
-    topics_list = list(articles.keys()) # ['sports', 'finance', 'politics']
-    topic = topics_list[num_page]
-
-    return HttpResponseRedirect(reverse('topic-page', args = [topic]))
+def simple_view(reqeust):
+    return render(request, '') #.html
 
 
