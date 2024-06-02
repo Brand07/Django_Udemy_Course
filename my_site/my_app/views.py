bb5f7cc597fe6be511c7f79f368447e3e146f8a0
Brand07 <81128304+Brand07@users.noreply.github.com>
2024-06-01 23:19:31 -0500

    html

--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 def example_view(request):
     # path - my_app/templates/my_app/example.html
     return render(request, 'my_app/example.html')
+
+def variable_view(request):
+
+    my_var = {'first_name': 'Brandon', 'last_name':'Yates',
+              'some_list' : [1,2,3], 'some_dict': {'inside_key': 'inside_value'}}
+
+    return render(request, 'my_app/variable.html', context = my_var)
